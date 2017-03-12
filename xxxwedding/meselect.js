170eae959389d600b522de86e3e0a56ef1a0ae18
yujuan <18158913588@163.com>
2017-03-12 09:36:17 +0800

    g

---
+++ xxxwedding/meselect.js
@@ -0,0 +1,158 @@
+;(function($,undefined){
+	var namespace = '.wpmessagecstselect';
+	$.widget('wp.messagecstselect',{
+		options : {
+			selectclass:'wp-diy-selected'//样式
+		}, // 配置项
+		_create : function(){
+			var self = this;
+			var selectclass=self.options.selectclass;
+			if(selectclass.length==0){ selectclass='wp-diy-selected';}
+			// 忽略多选的SELECT控件
+			if(self.element.prop("multiple")) return;
+			self._doinit(self).done(function(dom,selopts){
+				var $ctarget = dom,$diysel = $ctarget.next('.'+selectclass+'-outside').find('.'+selectclass+'');
+				// 设置默认值
+				setTimeout(function(){
+					var $cursel = $diysel.children('.'+selectclass+'-left'),difw = $cursel.siblings('.'+selectclass+'-button').outerWidth() + 2,
+					pad = self._int($cursel.css("paddingLeft")) + self._int($cursel.css("paddingRight")),finalw = $diysel.width() - difw - pad;
+					if (finalw <= 0) finalw = Math.max(65, $ctarget.width()) - pad - 16;
+					$cursel.width(finalw).attr("val",$ctarget.val()).html($ctarget.find('option:selected').html());$cursel = difw = finalw = null;
+				},50);
+				// 绑定CLICK事件
+				$diysel.bind('click'+namespace,function(e){
+					if(self.options.disabled || !selopts.length) return;
+					var $selectedcnt = $(selopts).appendTo('body'),maxheight = $selectedcnt.outerHeight(true);
+					self._selected($selectedcnt, $ctarget.val()); // 设置默认值
+					// 绑定LI:HOVER及CLICK事件
+					if ($selectedcnt.is(':hidden')) {
+						var $target = $(this);
+						$selectedcnt.find('li').bind('mouseenter'+namespace, function(e){
+							$(this).addClass('local').siblings().removeClass('local');
+						}).bind('mouseleave'+namespace, function(e){
+							$(this).removeClass('local');
+						}).bind('click'+namespace, function(e){
+							var $li = $(this),selval = $li.attr("val");
+							$target.children('.'+selectclass+'-left').html($li.html()).attr("val", selval);
+                                                     //   console.log('click:',$target);
+							$ctarget.val(selval).trigger('change');// 捕获已绑定SELECT事件
+						}).end().slideDown('slow',function(){
+							var $selcnt = $(this);
+							// 定位滚动条 2012/11/09
+							var selcnt = $selcnt[0],clienth = selcnt.clientHeight,$selected = $selcnt.find('li.local');
+							if ($selected.size() > 0) {
+								var seltop = $._parseFloat($selcnt.css("top")),
+								litop = seltop + $selected[0].offsetTop - selcnt.offsetTop + $selected.height();
+								if(clienth < litop) selcnt.scrollTop = litop - clienth;
+							}
+							// End
+							$(document).one('click'+namespace,function(e){
+								$selcnt.slideUp().remove();
+								return false;
+							});
+						});
+						setSeloptsPos($target,$selectedcnt,maxheight);
+						// 绑定RESIZE事件
+						$(window).resize(function(){
+							setSeloptsPos($target,$selectedcnt,maxheight);
+						});
+					}
+				});
+				function setSeloptsPos(target,dom,maxh){
+					var $target = target,pos = $target.offset();
+					dom.css({
+						"top": function(i,val){
+							var wnh = window.innerHeight||self.innerHeight||document.documentElement.clientHeight||document.body.clientHeight,
+							_top = self._int(pos.top),_oh = $target.outerHeight(true),newtop = 0;
+							if (wnh < _top + maxh + _oh) {
+								newtop = Math.max(0, _top - maxh - 1);
+								$(this).css({"border-bottom": 'none',"border-top": '1px solid #B5B5B5'});
+							} else newtop = _top + _oh + 1;
+							wnh = _top = _oh = null;
+							return newtop+'px';
+						},"left": function(i,val){
+							return self._int(pos.left+1)+'px';
+						}
+					});
+				}
+			});
+		},
+	    value : function(value){
+	    	var self = this;
+			var selectclass=self.options.selectclass;
+			if(selectclass.length==0){ selectclass='wp-diy-selected';}
+			var $target = self.element,$select = $target.next('.'+selectclass+'-outside');
+	    	$target.val(value);
+	    	$select.find('.'+selectclass+'-left').attr("val",value).html($target.children('option:selected').html());
+	    },
+		_doinit : function(dom){
+			var dtd = $.Deferred(),self = dom,$target = self.element;
+			var selectclass=self.options.selectclass,_stripslashes = function(str){
+				return (str + '').replace(/\\(.?)/g, function(s, $1){
+				    switch ($1) {
+				    	case '\\': return '\\';
+				    	case '0': return '\u0000';
+				    	case '': return '';
+				    	default: return $1;
+				    }
+			  	});
+			};
+			if(selectclass.length==0){ selectclass='wp-diy-selected';}
+			$target.hide();// 隐藏SELECT控件
+			// 自定义类SELECT
+			var selopt = '',selw = Math.max($target.width(), 65);
+                      //  console.log($target);
+			var selhtm = '<div class="'+selectclass+'-outside overz '+$target.attr('diyattr')+'" id="sel'+$target.attr('diyattr')+'" style="float:left;width:'+selw+'px;"><div class="'+selectclass+'">';
+			$target.children('option').each(function(i,c){
+				var self = c;
+				selopt += '<li val="'+self.value+'">'+_stripslashes(self.innerHTML)+'</li>';
+			});
+			selhtm += '<div class="'+selectclass+'-left" val="0">&nbsp;</div><div class="'+selectclass+'-button"><a href="###" onclick="return false;"></a></div></div></div>';
+			$target.after(selhtm);
+			dtd.resolve($target,'<div class="'+selectclass+'-content overz" id="opt_'+$target.attr('diyattr')+'" style="width:'+(selw-2)+'px;"><ul>'+selopt+'</ul></div>');
+			return dtd.promise();
+		},
+		_int : function(numstr){
+			var number = parseInt(numstr);
+			if(isNaN(number)) return 0;
+			return number;
+		},
+		_selected : function(dom,value){
+			dom.find('li').each(function(i,c){
+				var $target = $(c),ent = '';
+				ent = ($target.attr("val") == value)?'addClass':'removeClass';
+				$target[ent]('local');
+			});
+			return true;
+		},
+		enable : function(){
+			var self = this;
+			var selectclass=self.options.selectclass;
+			if(selectclass.length==0){ selectclass='wp-diy-selected';}
+			var $select = self.element.next('.'+selectclass+'-outside');
+			$select.find('.'+selectclass+'-left').css("color","#5A5A5A");
+			$select.find('.'+selectclass+'-button > a').removeClass('disabled');
+			$.Widget.prototype.enable.call(self);
+		},
+		disable : function(){
+			var self = this;
+			var selectclass=self.options.selectclass;
+			if(selectclass.length==0){ selectclass='wp-diy-selected';}
+			var $target = self.element,$select = $target.next('.'+selectclass+'-outside');
+			// 还原SELECT控件默认值
+			var firstopt = $target.find('option')[0];
+			$target[0].selectedIndex = 0;
+			$select.find('.'+selectclass+'-left').css("color","#7C7C7C").attr("val",firstopt.value).html(firstopt.innerHTML);
+			$select.find('.'+selectclass+'-button > a').addClass('disabled');
+			$.Widget.prototype.disable.call(self);
+		},
+		destroy : function(){
+			var self = this;
+			var selectclass=self.options.selectclass;
+			if(selectclass.length==0){ selectclass='wp-diy-selected';}
+			$(document).unbind(namespace);
+			self.element.next('.'+selectclass+'-outside').unbind(namespace).remove();
+			$.Widget.prototype.destroy.call(self);
+		}
+	});
+})(jQuery);
